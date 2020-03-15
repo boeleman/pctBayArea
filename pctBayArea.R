6ec008385cd06db51307e029063b42c75748cfa7
Arnout Boelens <ampboelens@gmail.com>
2020-03-14 19:09:22 -0700

    Added Bay Area heat map based on graphhopper router and 'go Dutch'

--- pctBayArea.R
+++ pctBayArea.R
@@ -7,22 +7,32 @@
 ##  "tidyverse",
 ##  "devtools",
 ##  "lwgeom",
+#  "simpleCache",
+#  "leaflet.providers",
+#  "leaflet",
+#  "rgeos",
 #  "cyclestreets",
 #  "pct",
-#  "stplanr",
 #  "sf",
 #  "geojsonsf"
 #)
 #remotes::install_cran(pkgs, lib="~/local/lib/R")
+#pkgs = c(
+#  "boeleman/stplanr"
+#)
+#remotes::install_github(pkgs, lib="~/local/lib/R")
 
 
 # Load libraries
-library(cyclestreets, lib.loc="~/local/lib/R")
-library(geojsonsf, lib.loc="~/local/lib/R")
-library(stplanr, lib.loc="~/local/lib/R")
-library(pct, lib.loc="~/local/lib/R")
-library(sf, lib.loc="~/local/lib/R")
-
+library(cyclestreets     , lib.loc="~/local/lib/R")
+library(htmlwidgets                               )
+library(geojsonsf        , lib.loc="~/local/lib/R")
+library(simpleCache      , lib.loc="~/local/lib/R")
+library(stplanr          , lib.loc="~/local/lib/R")
+library(leaflet          , lib.loc="~/local/lib/R")
+library(leaflet.providers, lib.loc="~/local/lib/R")
+library(pct              , lib.loc="~/local/lib/R")
+library(sf                                        )
 
 # Read all flow/od data
 AlamedaCountyBayArea_od      <- readr::read_delim("pct-inputs/02_intermediate/02_flow_data/AlamedaCounty-BayArea_od.csv"     , delim =",")
@@ -45,6 +55,10 @@ bayArea_od <- rbind(bayArea_od             , SantaClaraCountyBayArea_od  )
 bayArea_od <- rbind(bayArea_od             , SolanoCountyBayArea_od      )
 bayArea_od <- rbind(bayArea_od             , SonomaCountyBayArea_od      )
 
+# People going home
+bayArea_do <- bayArea_od
+bayArea_do[1:2] <- bayArea_do[2:1]
+bayArea_od <- rbind(bayArea_od,bayArea_do)
 
 # Read zones data
 bayArea_zones <- geojson_sf("pct-inputs/02_intermediate/01_geographies/bayArea_zones.GeoJson")
@@ -61,7 +75,6 @@ bayArea_lines = stplanr::od2line(flow = bayArea_od, zones = bayArea_zones)
 #plot(bayArea_zones$geometry, lwd=0.05)
 #plot(bayArea_lines["all"], lwd = bayArea_lines$all/250, add = TRUE)
 
-
 ## Filter data to more than zero bicycle trips
 #bayArea_lines <- bayArea_lines[bayArea_lines$bicycle > 0, ]
 
@@ -69,27 +82,36 @@ bayArea_lines = stplanr::od2line(flow = bayArea_od, zones = bayArea_zones)
 bayArea_lines$length     = as.numeric(sf::st_length(bayArea_lines))
 bayArea_lines$av_incline = 0.
 
-# Filter data to only length < 10000m
-bayArea_lines  <- bayArea_lines[bayArea_lines$length < 10000, ]
+# Filter data to only length < 20000m
+bayArea_lines  <- bayArea_lines[bayArea_lines$length < 20000, ]
 
-# Select only part of the data
-bayArea_lines <- bayArea_lines[1:10000,]
+## Select only part of the data
+#bayArea_lines <- bayArea_lines[1:10000,]
 
 # Compute length and av_incline of routes
-mytoken <- readLines("~/.cyclestreets/api-key")
-Sys.setenv(CYCLESTREET = mytoken)
+#mytoken <- readLines("~/.cyclestreets/api-key")
+#Sys.setenv(CYCLESTREET = mytoken)
+Sys.setenv(GRAPHHOPPER = "NULL")
 
-bayArea_routes <- stplanr::line2route(bayArea_lines, route_fun = route_cyclestreets, plan = "fastest")
+cacheDir = "."
+setCacheDir(cacheDir)
+
+simpleCache("bayArea_routes", {
+    bayArea_routes <- stplanr::line2route(bayArea_lines, route_fun = route_graphhopper, vehicle = "bike", base_url = "http://localhost:8989")
+#    bayArea_routes <- stplanr::line2route(bayArea_lines, route_fun = route_graphhopper, vehicle = "allbike", base_url = "http://localhost:8989")
+})
 
 # Compute distance
 bayArea_lines$length     <- bayArea_routes$length
 bayArea_lines$av_incline <- bayArea_routes$av_incline
 
-
 # Filter NA
 bayArea_lines  <- bayArea_lines[complete.cases(bayArea_lines$length), ]
 bayArea_routes <- bayArea_routes[complete.cases(bayArea_routes$length), ]
 
+# Filter data to only length < 25000m
+bayArea_lines  <- bayArea_lines[bayArea_lines$length < 25000, ]
+bayArea_routes <- bayArea_routes[bayArea_routes$length < 25000, ]
 
 # Compute current propensity to cycle
 breaks <- seq(0.0 , 30000.0, by=500.0)
@@ -115,6 +137,7 @@ plot(x = bayArea_lines$length, y = bayArea_lines$godutch_pcycle)
 points(x = labels, y = pcycle)
 
 
+simpleCache("rnet", {
 n_lines = 1000
 for (i in 1:ceiling(nrow(bayArea_lines)/n_lines))
 {
@@ -147,9 +170,23 @@ for (i in 1:ceiling(nrow(bayArea_lines)/n_lines))
         rnet = rbind(rnet, rnet0)
     }
 }
-
 rnet = stplanr::overline2(rnet, "godutch_slc")
+})
 
+# Replace values larger than 1000
+rnet$godutch_slc[rnet$godutch_slc > 2000 ] <- 2000
 
-# Plot density map of routes
-plot(rnet, lwd = log(rnet$godutch_slc / mean(rnet$godutch_slc)))
+# Only select ways with an increase > 400
+rnet <- rnet[rnet$godutch_slc > 399, ]
+
+## Plot density map of routes
+#plot(rnet, lwd = log(rnet$godutch_slc / mean(rnet$godutch_slc)))
+
+#pal = colorNumeric(palette = "RdYlBu", domain = c(0, max(rnet$godutch_slc)), reverse = TRUE)
+pal = colorNumeric(palette = "RdYlBu", domain = rnet$godutch_slc, reverse = TRUE)
+m <-  leaflet(data = rnet) %>% 
+    addPolylines(color = ~ pal(godutch_slc)) %>% 
+    addLegend(pal = pal, values = ~godutch_slc) %>% 
+    addProviderTiles(providers$CartoDB.Positron)
+
+saveWidget(m, file="bayArea.html")
